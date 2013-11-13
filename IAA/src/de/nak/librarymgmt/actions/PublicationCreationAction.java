a60c1eda34e83d425b4be3a34319662ac34bab15
mariokolbe <mario.kolbe@nordakademie.de>
2013-11-13 20:29:01 +0100

    PublicationCreation

--- IAA/src/de/nak/librarymgmt/actions/PublicationCreationAction.java
+++ IAA/src/de/nak/librarymgmt/actions/PublicationCreationAction.java
@@ -1,36 +1,55 @@
 package de.nak.librarymgmt.actions;
 
-import com.opensymphony.xwork2.ActionContext;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.opensymphony.xwork2.ActionSupport;
 
+import de.nak.librarymgmt.model.Keyword;
 import de.nak.librarymgmt.model.Publication;
+import de.nak.librarymgmt.model.PublicationType;
+import de.nak.librarymgmt.service.KeywordService;
 import de.nak.librarymgmt.service.PublicationService;
+import de.nak.librarymgmt.service.PublicationTypeService;
+import de.nak.librarymgmt.util.ConditionE;
 
 public class PublicationCreationAction extends ActionSupport {
 
 	private static final long serialVersionUID = 1L;
-	private PublicationService publicationService;
 	private Publication publicationBean;
+	private PublicationService publicationService;
+	private PublicationTypeService publicationTypeService;
+	private KeywordService keywordService;
 
 	@Override
 	public String execute() throws Exception {
 
-		publicationService.createPublication(publicationBean.getTitle(),
-				publicationBean.getAuthors(),
-				publicationBean.getPublicationDate(),
-				publicationBean.getCondition(),
-				publicationBean.getPublicationType(),
-				publicationBean.getKeywords(), publicationBean.getIsbn(),
-				publicationBean.getPublisher(), publicationBean.getIssue(),
-				publicationBean.getEdition());
+		keywordService.createKeyword("KW1");
+		keywordService.createKeyword("KW2");
+		Set<Keyword> keywords = new HashSet<Keyword>();
+		keywords.add(keywordService.findKeywordById(1L));
+		keywords.add(keywordService.findKeywordById(2L));
 
-		System.out.println("Publikation gespeichert");
+		publicationTypeService.createPublicationType("DUMMY");
+		PublicationType publicationType = publicationTypeService
+				.findPublicationTypeById(3L);
 
-		ActionContext.getContext().getSession().remove("publication");
-		System.out.println("Session cleared");
+		publicationService.createPublication("Dummy", new HashSet<String>(),
+				new Date(), ConditionE.NEW, publicationType, keywords, "Dummy",
+				"Dummy", "Dummy", "Dummy");
 
+		System.out.println("Publikation anlegen");
 		return "publicationSuccess";
 
+	}
+
+	public Publication getPublicationBean() {
+		return publicationBean;
+	}
+
+	public void setPublicationBean(Publication publicationBean) {
+		this.publicationBean = publicationBean;
 	}
 
 	public PublicationService getPublicationService() {
@@ -41,11 +60,20 @@ public class PublicationCreationAction extends ActionSupport {
 		this.publicationService = publicationService;
 	}
 
-	public Publication getPublicationBean() {
-		return publicationBean;
+	public PublicationTypeService getPublicationTypeService() {
+		return publicationTypeService;
 	}
 
-	public void setPublicationBean(Publication publicationBean) {
-		this.publicationBean = publicationBean;
+	public void setPublicationTypeService(
+			PublicationTypeService publicationTypeService) {
+		this.publicationTypeService = publicationTypeService;
+	}
+
+	public KeywordService getKeywordService() {
+		return keywordService;
+	}
+
+	public void setKeywordService(KeywordService keywordService) {
+		this.keywordService = keywordService;
 	}
 }
