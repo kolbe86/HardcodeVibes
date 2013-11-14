958b7dcc27e14de55cbd132a16373e7e321cf5f4
jonasmckl <jonas@jonasmackelsmbp>
2013-11-14 18:21:44 +0100

    Fehelden Klassen für Ausleiher anlegen und löschen

---
+++ IAA/src/de/nak/librarymgmt/actions/basicDataActions/AddBorrowerAction.java
@@ -0,0 +1,53 @@
+package de.nak.librarymgmt.actions.basicDataActions;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import de.nak.librarymgmt.model.Borrower;
+import de.nak.librarymgmt.service.BorrowerService;
+
+public class AddBorrowerAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private BorrowerService borrowerService;
+	private Borrower borrowerBean;
+	private List<Borrower> borrowers;
+
+	public String execute() throws Exception {
+
+		borrowerService.createBorrower(borrowerBean.getMatriculationNumber(),
+				borrowerBean.getFirstName(), borrowerBean.getLastName());
+
+		borrowers = borrowerService.listBorrowers();
+
+		System.out.println("Add BasicDataType Ende");
+		return "basicDataSuccess";
+
+	}
+
+	public BorrowerService getBorrowerService() {
+		return borrowerService;
+	}
+
+	public void setBorrowerService(BorrowerService borrowerService) {
+		this.borrowerService = borrowerService;
+	}
+
+	public Borrower getBorrowerBean() {
+		return borrowerBean;
+	}
+
+	public void setBorrowerBean(Borrower borrowerBean) {
+		this.borrowerBean = borrowerBean;
+	}
+
+	public List<Borrower> getBorrowers() {
+		return borrowers;
+	}
+
+	public void setBorrowers(List<Borrower> borrowers) {
+		this.borrowers = borrowers;
+	}
+}
---
+++ IAA/src/de/nak/librarymgmt/actions/basicDataActions/DeleteBorrowerAction.java
@@ -0,0 +1,52 @@
+package de.nak.librarymgmt.actions.basicDataActions;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import de.nak.librarymgmt.model.Borrower;
+import de.nak.librarymgmt.service.BorrowerService;
+
+public class DeleteBorrowerAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	private BorrowerService borrowerService;
+	private Borrower borrowerBean;
+	private List<Borrower> borrowers;
+
+	public String execute() throws Exception {
+
+		borrowerService.deleteBorrower(borrowerBean.getMatriculationNumber());
+
+		borrowers = borrowerService.listBorrowers();
+
+		System.out.println("Delete BasicDataType Ende");
+		return "basicDataSuccess";
+
+	}
+
+	public BorrowerService getBorrowerService() {
+		return borrowerService;
+	}
+
+	public void setBorrowerService(BorrowerService borrowerService) {
+		this.borrowerService = borrowerService;
+	}
+
+	public Borrower getBorrowerBean() {
+		return borrowerBean;
+	}
+
+	public void setBorrowerBean(Borrower borrowerBean) {
+		this.borrowerBean = borrowerBean;
+	}
+
+	public List<Borrower> getBorrowers() {
+		return borrowers;
+	}
+
+	public void setBorrowers(List<Borrower> borrowers) {
+		this.borrowers = borrowers;
+	}
+}
